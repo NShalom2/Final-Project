6bd943cc2efc6ed82c3682ed1c54a31a8ad0f327
NShalom2 <136999298+NShalom2@users.noreply.github.com>
2023-12-04 23:35:51 -0500

    Delete New folder directory

--- New folder/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#header").load("header.html");
-    $("#footer").load("footer.html");
-
-    $('.button-1').click(function () {
-        const product = $(this).closest('.product');
-        product.find('.additional-info').toggle(); // Toggle the visibility of additional info
-    });
-});
